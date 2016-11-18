84006480a8b5b73ce151304597d87476ed8b373c
swq <791437480@qq.com>
2016-11-18 15:14:35 +0800

    添加eventbus类库

--- app/src/main/java/com/weblink/zbcommunity/activity/SelectLocActivity.java
+++ app/src/main/java/com/weblink/zbcommunity/activity/SelectLocActivity.java
@@ -1,6 +1,8 @@
 package com.weblink.zbcommunity.activity;
 
-import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -10,21 +12,29 @@ import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
 
-import com.weblink.zbcommunity.BaseActivity;
+import com.amap.api.services.core.PoiItem;
+import com.amap.api.services.poisearch.PoiResult;
+import com.amap.api.services.poisearch.PoiSearch;
 import com.weblink.zbcommunity.R;
+import com.weblink.zbcommunity.adapter.BaseAdapter;
+import com.weblink.zbcommunity.adapter.PoiItemAdapter;
 import com.weblink.zbcommunity.utils.ToastUtils;
+import com.weblink.zbcommunity.views.MyLinearLayoutManager;
+import com.weblink.zbcommunity.widget.DividerItemDecoration;
+import com.weblink.zbcommunity.widget.LocationEvent;
+
+import org.greenrobot.eventbus.EventBus;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.BindView;
-import butterknife.ButterKnife;
 
 
 /**
  * Created by swq on 2016/11/11.
  */
-public class SelectLocActivity extends BaseActivity  {
+public class SelectLocActivity extends BaseActivity implements TextWatcher, PoiSearch.OnPoiSearchListener {
     @BindView(R.id.tv_left)
     TextView tvLeft;
     @BindView(R.id.tv_title)
@@ -46,16 +56,14 @@ public class SelectLocActivity extends BaseActivity  {
     LinearLayout llCity;
     @BindView(R.id.tv_search)
     AutoCompleteTextView keyWorldsView;
-
+    @BindView(R.id.locts)
+    RecyclerView lvLocts;
 
     private ArrayAdapter cityAdapter;
     private ArrayAdapter townAdapter;
     private List<String> cityList;
     private List<String> townList;
-
-    private ArrayAdapter<String> sugAdapter = null;
-
-    private List<String> suggest;
+    private PoiItemAdapter poiItemAdapter;
 
 
     @Override
@@ -68,11 +76,16 @@ public class SelectLocActivity extends BaseActivity  {
     public void initView() {
 
 
-//        ButterKnife.bind(this);
         tvTitle.setText("定位选择");
         tvTitle.setVisibility(View.VISIBLE);
         ivLeft.setVisibility(View.VISIBLE);
         ivLeft.setImageResource(R.drawable.back_normal);
+        tvCurrLoc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
 
 
         initSpinner();
@@ -81,13 +94,29 @@ public class SelectLocActivity extends BaseActivity  {
 
         initPoiSearch();
 
-        tvCurrLoc.setOnClickListener(new View.OnClickListener() {
+        initLocDatas();
+
+    }
+
+    private void initLocDatas() {
+        MyLinearLayoutManager linearLayoutManager = new MyLinearLayoutManager(this);
+        lvLocts.setLayoutManager(linearLayoutManager);
+        lvLocts.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL_LIST));
+
+        poiItemAdapter = new PoiItemAdapter(this, poiItems);
+        lvLocts.setAdapter(poiItemAdapter);
+
+        poiItemAdapter.setOnItemClickListener(new BaseAdapter.OnItemClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onItemClick(View view, int position) {
+
+
+                EventBus.getDefault().post(new LocationEvent(poiItems.get(position).getCityName(),
+                        poiItems.get(position).getTitle()));
+
                 finish();
             }
         });
-
     }
 
     private void initPoiSearch() {
@@ -96,13 +125,15 @@ public class SelectLocActivity extends BaseActivity  {
     }
 
 
-
     private void initAutoSearch() {
 
-        String [] arr={"aa","aab","aac","aac","aac","aac","aac","aac","aac","aac","aac","aac","aac"};
-        ArrayAdapter arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,arr);
-        keyWorldsView.setThreshold(1);
-        keyWorldsView.setAdapter(arrayAdapter);
+//        String [] arr={"aa","aab","aac","aac","aac","aac","aac","aac","aac","aac","aac","aac","aac"};
+//        ArrayAdapter arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,arr);
+//        keyWorldsView.setThreshold(1);
+//        keyWorldsView.setAdapter(arrayAdapter);
+
+
+        keyWorldsView.addTextChangedListener(this);// 添加文本输入框监听事件
     }
 
     private void initSpinner() {
@@ -164,11 +195,63 @@ public class SelectLocActivity extends BaseActivity  {
 
     }
 
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        // TODO: add setContentView(...) invocation
-        ButterKnife.bind(this);
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+
+        String newText = s.toString().trim();
+        doSearchQuery(newText);
+
+    }
+
+    private int currentPage;
+    private PoiSearch.Query query;// Poi查询条件类
+    private PoiSearch poiSearch;// POI搜索
+    private PoiResult poiResult; // poi返回的结果
+    private List<PoiItem> poiItems = new ArrayList<PoiItem>();
+
+    public void doSearchQuery(String key) {
+//        showProgressDialog();// 显示进度框
+        currentPage = 0;
+        query = new PoiSearch.Query(key, "", "淄博市");// 第一个参数表示搜索字符串，第二个参数表示poi搜索类型，第三个参数表示poi搜索区域（空字符串代表全国）
+        query.setPageSize(100);// 设置每页最多返回多少条poiitem
+        query.setPageNum(0);// 设置查第一页
+        poiSearch = new PoiSearch(this, query);
+        poiSearch.setOnPoiSearchListener(this);
+        poiSearch.searchPOIAsyn();
+    }
+
+    @Override
+    public void onPoiSearched(PoiResult result, int rCode) {
+
+        if (rCode == 1000) {
+            if (result != null && result.getQuery() != null) {// 搜索poi的结果
+                poiResult = result;
+                // 取得搜索到的poiitems有多少页
+                poiItems.clear();
+                List<PoiItem> pois = poiResult.getPois();// 取得第一页的poiitem数据，页数从数字0开始
+
+                if (pois != null && pois.size() > 0)
+                    poiItems.addAll(pois);
+                poiItemAdapter.notifyDataSetChanged();
+            }
+        } else {
+
+            ToastUtils.showToast(this, "请检查您的网络");
+        }
+    }
+
+    @Override
+    public void onPoiItemSearched(PoiItem poiItem, int rCode) {
+
     }
 }
--- app/src/main/java/com/weblink/zbcommunity/adapter/CommkindlvAdapter.java
+++ app/src/main/java/com/weblink/zbcommunity/adapter/CommkindlvAdapter.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
